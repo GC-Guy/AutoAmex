b5249a332021db38774075c985d5c3ebbec9005f
GC-Guy <gcguy@shhhmail.com>
2018-06-01 08:16:50 -0700

    various fixes

--- src/autoamexalloffers.py
+++ src/autoamexalloffers.py
@@ -2,15 +2,14 @@
 
 from selenium import webdriver
 from datetime import datetime
-import sys
-import time
-from datetime import datetime as dt
-from helper import loadConfig, closeFeedback, clickOnAddedToCard, clickOnLoadMore, amexLogIn, amexLogOut, getDriver
+import sys, time, pickle
+# from datetime import datetime as dt
+from helper import *
 
 amexWebsite = "https://online.americanexpress.com/myca/logon/us/action/LogonHandler?request_type=LogonHandler&Face=en_US&inav=iNavLnkLog"
 
 
-def getAddedOffers(username, password, outputlog = True, browser = "Chrome"):
+def getAddedOffers(username, password, lastfive, nickname, outputlog = True, browser = "Chrome"):
   # re-route output
   orig_stdout = sys.stdout
   logfile = None
@@ -20,6 +19,14 @@ def getAddedOffers(username, password, outputlog = True, browser = "Chrome"):
     logfilename = "offers " + logfilename.replace(':', '_') + ".csv"
     logfile = open('../tmp/' + logfilename, 'w+')
     sys.stdout = logfile
+  donefilename = datetime.strftime(datetime.now(), '%Y-%m-%d')
+  try:
+    with open('../junk/'+donefilename+'.pickle', 'rb') as f:  # Python 3: open(..., 'rb')
+      doneuser = pickle.load(f)
+      # print(lastcards)
+  except:
+    doneuser = {"one": "two"}
+
   # input error handle
   if username == [] or password == [] or len(username) != len(password):
     print "username array does not have the same length as password array..."
@@ -41,6 +48,16 @@ def getAddedOffers(username, password, outputlog = True, browser = "Chrome"):
 
   # loop through all username/password combinations
   for idx in range(len(username)):
+    if username[idx] in doneuser:
+      continue
+    time.sleep(15)
+    sys.stdout = orig_stdout
+    sys.stdout.write('\r                                            \rRunning ' + username[idx])
+    sys.stdout.flush()
+    #print "\rRunning " + username[idx],
+    if outputlog:
+      sys.stdout = logfile
+
     eachbegintime = time.time()
     try:
       driver.get(amexWebsite)
@@ -54,17 +71,21 @@ def getAddedOffers(username, password, outputlog = True, browser = "Chrome"):
     # fill and submit login form
     try:
       amexLogIn(driver, username[idx], password[idx])
+#      print username[idx]
     except:
       print "username/password combination is incorrect..."
+#      print username[idx]
       continue
     time.sleep(2)
-    closeFeedback(driver)
-    clickOnAddedToCard(driver)
-    clickOnLoadMore(driver)
-    time.sleep(2)
-    closeFeedback(driver)
+#    closeFeedback(driver)
+#    clickOnAddedToCard(driver)
+#    closeFeedback(driver)
+#    clickOnLoadMore(driver)
+#    time.sleep(2)
+#    closeFeedback(driver)
 
     # main program
+    driver.get(added_page)
     offers = driver.find_elements_by_xpath('//*[@id="offers"]/div/section[2]/section/div')
     offers.pop(0)
     offerstext = [offer.text.encode('utf-8') for offer in offers]
@@ -111,20 +132,25 @@ def getAddedOffers(username, password, outputlog = True, browser = "Chrome"):
       amexLogOut(driver)
     except:
       pass
+    doneuser[username[idx]] = password[idx]
+    with open('../junk/'+donefilename+'.pickle', 'wb') as f:  # Python 3: open(..., 'rb')
+      pickle.dump(doneuser, f)
+
     time.sleep(1)
 
   majorDateOfferList = list(majorDateOfferPair)
   majorDateOfferList.sort(key=lambda tup:tup[0])
   # write 1st line
+  sys.stdout.write(',,')
   for dateOffer in majorDateOfferList:
     offer = majorOfferDollarMap[dateOffer[1]].replace(',', ' and')
     sys.stdout.write(','+offer)
-  sys.stdout.write('\n')
+  sys.stdout.write('\n,,')
   # write 2nd line
   for dateOffer in majorDateOfferList:
     desc = majorOfferDescMap[dateOffer[1]].replace(',', ' and')
     sys.stdout.write(','+desc)
-  sys.stdout.write('\n')
+  sys.stdout.write('\n,,')
   # write 3rd line
   for dateOffer in majorDateOfferList:
     date = majorOfferDateMap[dateOffer[1]]
@@ -133,6 +159,8 @@ def getAddedOffers(username, password, outputlog = True, browser = "Chrome"):
   # write the rest
   for i in range(len(username)):
     sys.stdout.write(username[i])
+    sys.stdout.write("," + lastfive[i])
+    sys.stdout.write("," + nickname[i])
     for dateOffer in majorDateOfferList:
       if dateOffer[1] in userOffersList[i]:
         sys.stdout.write(',+')
@@ -150,8 +178,13 @@ def getAddedOffers(username, password, outputlog = True, browser = "Chrome"):
 
 
 def main():
-  username, password = loadConfig("../conf/config.csv")
-  getAddedOffers(username, password, outputlog = True)
+  try:
+    username, password, lastfive, nickname = loadConfig2("../conf/config2.csv")
+  except:
+    username, password = loadConfig("../conf/config.csv")
+    lastfive = []
+    nickname = []
+  getAddedOffers(username, password, lastfive, nickname, outputlog = True)
 
 if __name__ == '__main__':
   main()
